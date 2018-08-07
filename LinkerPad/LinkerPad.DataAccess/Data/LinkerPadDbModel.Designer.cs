804e5f60df316c4f4b04478e010dfc4ba1bb9e7b
mohammad <mohammad.abolhasany@gmail.com>
2018-08-07 18:22:52 +0430

    updated project .net framework

--- LinkerPad/LinkerPad.DataAccess/Data/LinkerPadDbModel.Designer.cs
+++ LinkerPad/LinkerPad.DataAccess/Data/LinkerPadDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Rayo\LinkerPad.DataAccess\Data\LinkerPadDbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\mohammad\Documents\GitHub\LinkerPadBack\LinkerPad\LinkerPad.DataAccess\Data\LinkerPadDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
