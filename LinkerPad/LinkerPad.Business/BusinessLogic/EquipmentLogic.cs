1695d12e75e58b86d8329ffa084f70539ac0ce38
Linkerpad <39854494+LinkerpadStartup@users.noreply.github.com>
2018-08-25 14:32:40 +0430

    logic equipment,material

--- LinkerPad/LinkerPad.Business/BusinessLogic/EquipmentLogic.cs
+++ LinkerPad/LinkerPad.Business/BusinessLogic/EquipmentLogic.cs
@@ -18,5 +18,63 @@ namespace LinkerPad.Business.BusinessLogic
             _unitOfWork = unitOfWork;
             _equipmentRepository = equipmentRepository;
         }
+        public void Add(EquipmentData equipmentData)
+        {
+            _unitOfWork.BeginTransaction();
+
+            _equipmentRepository.Create(equipmentData);
+
+            _unitOfWork.Commit();
+        }
+
+        public void Edit(EquipmentData equipmentData)
+        {
+            _unitOfWork.BeginTransaction();
+
+            EquipmentData currentEquipmentData = _equipmentRepository.GetById(equipmentData.Id);
+
+            currentEquipmentData.Description = equipmentData.Description;
+            currentEquipmentData.Title = equipmentData.Title;
+            currentEquipmentData.WorkHours = equipmentData.WorkHours;
+            currentEquipmentData.NumberOfActiveEquipment = equipmentData.NumberOfActiveEquipment;
+            currentEquipmentData.NumberOfDeactiveEquipment = equipmentData.NumberOfDeactiveEquipment;
+            currentEquipmentData.ModifiedDate = DateTime.Now;
+
+            _equipmentRepository.Update(currentEquipmentData);
+
+            _unitOfWork.Commit();
+        }
+
+        public void Delete(Guid equipmentId)
+        {
+            _unitOfWork.BeginTransaction();
+
+            _equipmentRepository.Delete(equipmentId);
+
+            _unitOfWork.Commit();
+        }
+
+        public EquipmentData GetEquipment(Guid equipmentId)
+        {
+            return _equipmentRepository.GetById(equipmentId);
+        }
+
+        public IEnumerable<EquipmentData> GetProjectEquipment(Guid projectId, DateTime reportDate)
+        {
+            IQueryable<EquipmentData> equipmentDataSource = _equipmentRepository.GetAll()
+                 .Where(d => d.ProjectData.Id == projectId && d.ReportDate.Date == reportDate.Date);
+
+            return equipmentDataSource.AsEnumerable();
+        }
+
+        public bool IsEquipmentCreatedBy(Guid currentUserId, Guid equipmentId)
+        {
+            return _equipmentRepository.GetById(equipmentId).CreatedBy.Id == currentUserId;
+        }
+
+        public bool IsEquipmentExist(Guid projectId, Guid equipmentId)
+        {
+            return _equipmentRepository.GetAll().Any(d => d.ProjectData.Id == projectId && d.Id == equipmentId);
+        }
     }
 }
--- LinkerPad/LinkerPad.Business/BusinessLogic/MaterialLogic.cs
+++ LinkerPad/LinkerPad.Business/BusinessLogic/MaterialLogic.cs
@@ -17,6 +17,63 @@ namespace LinkerPad.Business.BusinessLogic
         {
             _unitOfWork = unitOfWork;
             _materialRepository = materialRepository;
-        } 
+        }
+        public void Add(MaterialData materialData)
+        {
+            _unitOfWork.BeginTransaction();
+
+            _materialRepository.Create(materialData);
+
+            _unitOfWork.Commit();
+        }
+
+        public void Edit(MaterialData materialData)
+        {
+            _unitOfWork.BeginTransaction();
+
+            MaterialData currentMaterialData = _materialRepository.GetById(materialData.Id);
+
+            currentMaterialData.Description = materialData.Description;
+            currentMaterialData.Title = materialData.Title;
+            currentMaterialData.ConsumedQuantity = materialData.ConsumedQuantity;
+            currentMaterialData.ConsumedQuantityUnit = materialData.ConsumedQuantityUnit;
+            currentMaterialData.ModifiedDate = DateTime.Now;
+
+            _materialRepository.Update(currentMaterialData);
+
+            _unitOfWork.Commit();
+        }
+
+        public void Delete(Guid materialId)
+        {
+            _unitOfWork.BeginTransaction();
+
+            _materialRepository.Delete(materialId);
+
+            _unitOfWork.Commit();
+        }
+
+        public MaterialData GetMaterial(Guid materialId)
+        {
+            return _materialRepository.GetById(materialId);
+        }
+
+        public IEnumerable<MaterialData> GetProjectMaterials(Guid projectId, DateTime reportDate)
+        {
+            IQueryable<MaterialData> materialsDataSource = _materialRepository.GetAll()
+                 .Where(d => d.ProjectData.Id == projectId && d.ReportDate.Date == reportDate.Date);
+
+            return materialsDataSource.AsEnumerable();
+        }
+
+        public bool IsMaterialCreatedBy(Guid currentUserId, Guid materialId)
+        {
+            return _materialRepository.GetById(materialId).CreatedBy.Id == currentUserId;
+        }
+
+        public bool IsMaterialExist(Guid projectId, Guid materialId)
+        {
+            return _materialRepository.GetAll().Any(d => d.ProjectData.Id == projectId && d.Id == materialId);
+        }
     }
 }
