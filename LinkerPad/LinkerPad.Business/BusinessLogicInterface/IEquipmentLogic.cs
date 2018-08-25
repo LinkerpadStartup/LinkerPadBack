50d13b14472b83ab3d00f56bf77b0bdf8e567515
Linkerpad <39854494+LinkerpadStartup@users.noreply.github.com>
2018-08-25 14:32:12 +0430

    interface equipment,material

--- LinkerPad/LinkerPad.Business/BusinessLogicInterface/IEquipmentLogic.cs
+++ LinkerPad/LinkerPad.Business/BusinessLogicInterface/IEquipmentLogic.cs
@@ -6,6 +6,18 @@ namespace LinkerPad.Business.BusinessLogicInterface
 {
     public interface IEquipmentLogic
     {
-       
+        void Add(EquipmentData equipmentData);
+
+        void Edit(EquipmentData equipmentData);
+
+        void Delete(Guid equipmentId);
+
+        EquipmentData GetEquipment(Guid equipmentId);
+
+        IEnumerable<EquipmentData> GetProjectEquipment(Guid projectId, DateTime reportDate);
+
+        bool IsEquipmentCreatedBy(Guid currentUserId, Guid equipmentId);
+
+        bool IsEquipmentExist(Guid projectId, Guid equipmentId);
     }
 }
--- LinkerPad/LinkerPad.Business/BusinessLogicInterface/IMaterialLogic.cs
+++ LinkerPad/LinkerPad.Business/BusinessLogicInterface/IMaterialLogic.cs
@@ -6,6 +6,18 @@ namespace LinkerPad.Business.BusinessLogicInterface
 {
     public interface IMaterialLogic
     {
-       
+        void Add(MaterialData materialData);
+
+        void Edit(MaterialData materialData);
+
+        void Delete(Guid materialId);
+
+        MaterialData GetMaterial(Guid materialId);
+
+        IEnumerable<MaterialData> GetProjectMaterials(Guid projectId, DateTime reportDate);
+
+        bool IsMaterialCreatedBy(Guid currentUserId, Guid materialId);
+
+        bool IsMaterialExist(Guid projectId, Guid materialId);
     }
 }
