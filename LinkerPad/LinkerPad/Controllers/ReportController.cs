9df28aea6388934294bc6f17d97fa5f38f02b1af
Linkerpad <39854494+LinkerpadStartup@users.noreply.github.com>
2018-09-08 17:25:08 +0430

    report&css notes

--- LinkerPad/LinkerPad/Controllers/ReportController.cs
+++ LinkerPad/LinkerPad/Controllers/ReportController.cs
@@ -7,6 +7,7 @@ using LinkerPad.Data;
 using LinkerPad.Models.DailyActivity;
 using LinkerPad.Models.Equipment;
 using LinkerPad.Models.Material;
+using LinkerPad.Models.Notes;
 using LinkerPad.Models.Report;
 using Rotativa;
 
@@ -17,12 +18,15 @@ namespace LinkerPad.Controllers
         private readonly IDailyActivityLogic _dailyActivityLogic;
         private readonly IMaterialLogic _materialLogic;
         private readonly IEquipmentLogic _equipmentLogic;
+        private readonly INotesLogic _notesLogic;
 
-        public ReportController(IDailyActivityLogic dailyActivityLogic, IMaterialLogic materialLogic, IEquipmentLogic equipmentLogic)
+
+        public ReportController(IDailyActivityLogic dailyActivityLogic, IMaterialLogic materialLogic, IEquipmentLogic equipmentLogic, INotesLogic notesLogic)
         {
             _dailyActivityLogic = dailyActivityLogic;
             _materialLogic = materialLogic;
             _equipmentLogic = equipmentLogic;
+            _notesLogic = notesLogic;
         }
 
         public ActionResult CreatePdfReport(Guid projectId, DateTime reportDate)
@@ -38,12 +42,15 @@ namespace LinkerPad.Controllers
             IList<DailyActivityData> dailyActivityDatas = _dailyActivityLogic.GetProjectDailyActivies(projectId, reportDate).ToList();
             IList<MaterialData> materialDatas = _materialLogic.GetProjectMaterials(projectId, reportDate).ToList();
             IList<EquipmentData> equipmentDatas = _equipmentLogic.GetProjectEquipment(projectId, reportDate).ToList();
+            IList<NotesData> notesDatas = _notesLogic.GetProjectNotes(projectId, reportDate).ToList();
+
 
             ReportViewModel reportViewModel = new ReportViewModel
             {
                 DailyActivitesViewModel = dailyActivityDatas.Select(DailyActivityViewModel.GetDailyActivityViewModel).ToList(),
                 MaterialsViewModel = materialDatas.Select(MaterialViewModel.GetMaterialViewModel).ToList(),
-                EquipmentViewModel = equipmentDatas.Select(EquipmentViewModel.GetEquipmentViewModel).ToList()
+                EquipmentViewModel = equipmentDatas.Select(EquipmentViewModel.GetEquipmentViewModel).ToList(),
+                NotesViewModel = notesDatas.Select(NotesViewModel.GetNotesViewModel).ToList()
             };
 
             return View(reportViewModel);
--- LinkerPad/LinkerPad/Models/Report/ReportViewModel.cs
+++ LinkerPad/LinkerPad/Models/Report/ReportViewModel.cs
@@ -2,6 +2,8 @@
 using LinkerPad.Models.DailyActivity;
 using LinkerPad.Models.Equipment;
 using LinkerPad.Models.Material;
+using LinkerPad.Models.Notes;
+
 
 namespace LinkerPad.Models.Report
 {
@@ -12,6 +14,9 @@ namespace LinkerPad.Models.Report
         public IList<MaterialViewModel> MaterialsViewModel { get; set; }
 
         public IList<EquipmentViewModel> EquipmentViewModel { get; set; }
+
+        public IList<NotesViewModel> NotesViewModel { get; set; }
+
 
         public bool OfficialReport { get; set; }
     }
